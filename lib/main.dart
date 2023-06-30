61f2136868e2b8cd85aed0fcce12ff4d1b6a93a3
R. Fadhil <ridhan.fadhilah@sg-dsa.com>
2023-06-30 20:03:40 +0700

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: const Text("Fadhil App")),
+        body: Stack(
+          children: <Widget>[
+            Column(
+              children: <Widget>[
+                Flexible(
+                    flex: 1,
+                    child: Row(
+                      children: <Widget>[
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.white,
+                            )),
+                        Flexible(
+                            flex: 1,
+                            child: Container(
+                              color: Colors.black,
+                            )),
+                      ],
+                    )),
+                const Flexible(flex: 1, child: Text("Aku"))
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
